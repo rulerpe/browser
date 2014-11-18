57d8445d49b02ce4bf51a023a13ee736bafffb8b
Peter <neetisthebest@gmail.com>
2014-11-17 23:10:27 -0800

    Finished

--- client/client.rb
+++ client/client.rb
@@ -1,16 +1,33 @@
 require 'socket'
+require 'json'
 
 hostname = 'localhost'
 port = 2000
-path = "/index.html"
-
+index_path = "/index.html"
+post_path = "/thanks.html"
 
 s = TCPSocket.open(hostname,port)
-puts "test1"
-request = "Get #{path} HTTP/1.0\r\n\r\n"
-puts "test2"
-s.print(request)
-puts "test3"
+print "1,GET or 2,POST: "
+chossice = gets.chomp
+if chossice == '1'
+#	request = "Get #{index_path} HTTP/1.0\r\n\r\n"
+	get = {:r => "Get #{index_path} HTTP/1.0\r\n\r\n"}
+	puts get[:r]
+	s.print get.to_json
+	s.print "\n"
+	
+else
+	viking = Hash.new
+	print "please enter your name: "
+	name = gets.chomp
+	print "enter your email: "
+	email = gets.chomp
+	request ="POST #{post_path} HTTP/1.0\r\n\r\n"
+	viking = {:r => request, :viking => {:name => name, :email => email}}
+	s.print(viking.to_json + "\n")
+
+
+end
 while line = s.gets   # Read lines from the socket
   puts line.chop      # And print with platform line terminator
 end
--- server/server.rb
+++ server/server.rb
@@ -1,26 +1,40 @@
 require 'socket'
+require 'json'
+require 'erb'
 
 server = TCPServer.open(2000)
+index_path = "index.html"
+post_path = "thanks.html"
 loop{
 client = server.accept
-puts "test1"
+
 initial_line = client.gets
 p initial_line
-puts "test2"
-arr_line = initial_line.split
-puts "test3"
+from = JSON.parse(initial_line)
+#from.each{|key,value| puts "#{key},#{value}"}
+p from['r']
+arr_line =from['r'].lines.first.split
 arr_line[1].delete! "/"
-puts "test4"
 if File.exists?(arr_line[1])
+	puts arr_line[1]
 	file = File.open(arr_line[1],"r")
 	client.puts "#{arr_line[2]} 200 OK"
 	puts "has file"
 	client.puts("Date: " + Time.now.ctime)
 	client.puts("Content-Type: text/html")
 	content = file.read
-	client.puts("Content-Length: #{content.length}")
-	
-	client.puts (content)
+	if arr_line[1] == index_path
+		client.puts("Content-Length: #{content.length}")
+		client.puts (content)
+	elsif arr_line[1] == post_path
+		f = File.read(arr_line[1])
+		erb_template = ERB.new f
+		name = from['viking']['name']
+		email = from['viking']['email']
+		form_letter = erb_template.result(binding)
+		client.puts (form_letter)
+	end
+		
 else
 	client.puts "#{arr_line[2]} 404 Not Found"
 end
